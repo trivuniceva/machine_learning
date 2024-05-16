970918383de75b3b307c4b90a3bfd9d55bd26ccf
trivuniceva <trivunic99@gmail.com>
2024-05-16 15:39:21 +0200

    Update main.py

--- Z4/main.py
+++ Z4/main.py
@@ -5,9 +5,13 @@ from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score
 from sklearn.preprocessing import LabelEncoder
+import sys
 
 
-data = pd.read_csv('train.csv')
+train_path = sys.argv[1]
+test_path = sys.argv[2]
+
+data = pd.read_csv(train_path)
 
 label_encoder = LabelEncoder()
 data['Gaming_Platform'] = label_encoder.fit_transform(data['Gaming_Platform'])
@@ -28,4 +32,5 @@ rf_classifier.fit(X, y)
 y_pred = rf_classifier.predict(X)
 f1_macro = f1_score(y, y_pred, average='macro')
 
-print("Makro F1 mera:", f1_macro)
+
+print(f1_macro)
