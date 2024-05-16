4c87842291b3d1bb1c6f6b94f46a0fe8c1885d7b
trivuniceva <trivunic99@gmail.com>
2024-05-16 15:20:43 +0200

    Z4
    at this one I get F1 = 1; but this is suspicious

---
+++ Z4/main.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+from sklearn.impute import SimpleImputer
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import f1_score
+from sklearn.preprocessing import LabelEncoder
+
+
+data = pd.read_csv('train.csv')
+
+label_encoder = LabelEncoder()
+data['Gaming_Platform'] = label_encoder.fit_transform(data['Gaming_Platform'])
+data['Genre'] = label_encoder.fit_transform(data['Genre'])
+
+imputer = SimpleImputer(strategy='mean')
+data_filled = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
+
+pca = PCA(n_components=7)
+data_reduced = pca.fit_transform(data_filled)
+
+X = data_reduced
+y = data['Genre']
+
+rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+rf_classifier.fit(X, y)
+
+y_pred = rf_classifier.predict(X)
+f1_macro = f1_score(y, y_pred, average='macro')
+
+print("Makro F1 mera:", f1_macro)
