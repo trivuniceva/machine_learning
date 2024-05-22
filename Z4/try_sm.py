4922c302c2c00fccd8fa7803718c2b041f618913
trivuniceva <trivunic99@gmail.com>
2024-05-22 19:32:10 +0200

    This one is better

--- Z4/try_sm.py
+++ Z4/try_sm.py
@@ -7,64 +7,43 @@ from sklearn.metrics import f1_score
 from sklearn.preprocessing import LabelEncoder
 import sys
 
-# Učitavanje trening i test podataka iz argumenata komandne linije
 train_path = sys.argv[1]
 test_path = sys.argv[2]
 
 data = pd.read_csv(train_path)
 test_data = pd.read_csv(test_path)
 
-# Kodiranje tekstualnih atributa za trening i test podatke
 label_encoder = LabelEncoder()
-all_genres = pd.concat([data['Genre'], test_data['Genre']], axis=0)
-label_encoder.fit(all_genres)
-
-# Provera da li postoje neviđene oznake u test podacima
-# unseen_labels = set(test_data['Genre']) - set(data['Genre'])
-# if unseen_labels:
-#     raise ValueError("Test podaci sadrže neviđene oznake za žanrove: {}".format(unseen_labels))
-
+label_encoder.fit(data['Genre'])
 data['Genre'] = label_encoder.transform(data['Genre'])
 test_data['Genre'] = label_encoder.transform(test_data['Genre'])
 
-# Kodiranje tekstualnih atributa za trening i test podatke
 label_encoder_platform = LabelEncoder()
-all_platforms = pd.concat([data['Gaming_Platform'], test_data['Gaming_Platform']], axis=0)
-label_encoder_platform.fit(all_platforms)
-
-# Transformacija platformi
+label_encoder_platform.fit(data['Gaming_Platform'])
 data['Gaming_Platform'] = label_encoder_platform.transform(data['Gaming_Platform'])
 test_data['Gaming_Platform'] = label_encoder_platform.transform(test_data['Gaming_Platform'])
 
-# Priprema trening podataka
 imputer = SimpleImputer(strategy='mean')
 data_filled = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
+test_filled = pd.DataFrame(imputer.transform(test_data), columns=test_data.columns)
 
-# Redukcija dimenzionalnosti na trening podacima
-pca = PCA(n_components=7)
-data_reduced = pca.fit_transform(data_filled)
+n_features = data_filled.shape[1] - 1
+n_components = min(7, n_features)
 
-# Podela trening podataka na X_train i y_train
+pca = PCA(n_components=n_components)
+data_reduced = pca.fit_transform(data_filled.drop(columns='Genre'))
+test_reduced = pca.transform(test_filled.drop(columns='Genre'))
+
 X_train = data_reduced
-y_train = data['Genre']
+y_train = data_filled['Genre']
 
-# Izgradnja ansambla klasifikatora (ovde koristimo Random Forest)
+X_test = test_reduced
+y_test = test_filled['Genre']
+
 rf_classifier = RandomForestClassifier(n_estimators=100, random_state=42)
 rf_classifier.fit(X_train, y_train)
 
-# Priprema test podataka
-test_filled = pd.DataFrame(imputer.transform(test_data), columns=test_data.columns)
-
-# Redukcija dimenzionalnosti na test podacima
-test_reduced = pca.transform(test_filled)
-
-# Podela test podataka na X_test i y_test
-X_test = test_reduced
-y_test = test_data['Genre']
-
-# Predikcija na test podacima
 y_pred_test = rf_classifier.predict(X_test)
-
 f1_macro_test = f1_score(y_test, y_pred_test, average='macro')
 
 print(f1_macro_test)
