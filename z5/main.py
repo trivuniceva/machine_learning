76422eac944a925ddf06a47d3017afd6a8d5721a
trivuniceva <trivunic99@gmail.com>
2024-05-30 18:57:17 +0200

    clean outliers

--- z5/main.py
+++ z5/main.py
@@ -1,14 +1,14 @@
 import pandas as pd
 import numpy as np
-from sklearn.impute import SimpleImputer
-from sklearn.decomposition import PCA
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
-from sklearn.preprocessing import LabelEncoder
-import sys
-import seaborn as sns
-import matplotlib.pyplot as plt
 from sklearn.impute import KNNImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.mixture import GaussianMixture
+from sklearn.metrics import v_measure_score
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import sys
+from sklearn.cluster import KMeans
 
 
 train_path = sys.argv[1]
@@ -40,5 +40,43 @@ data_imputed['region'] = data['region']
 
 print(data_imputed.isnull().sum())
 
+scaler = StandardScaler()
+features = data_imputed.drop(columns=['region'])
+features_scaled = scaler.fit_transform(features)
+
+print(features_scaled)
+
+kmeans = KMeans(n_clusters=4, random_state=42)
+data['Cluster'] = kmeans.fit_predict(features_scaled)
 
 
+pca = PCA(n_components=2)
+pca_components = pca.fit_transform(features_scaled)
+
+# Plotovanje rezultata
+plt.figure(figsize=(10, 7))
+plt.scatter(pca_components[:, 0], pca_components[:, 1], c=data['Cluster'], cmap='viridis', s=50, alpha=0.7)
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
+plt.title('PCA plot podataka')
+plt.colorbar(label='Klaster')
+plt.show()
+
+
+outlier_indices = detect_outliers_zscore(data_imputed, features.columns)
+print("Outlier indices:", outlier_indices)
+
+plt.figure(figsize=(10, 7))
+plt.scatter(pca_components[:, 0], pca_components[:, 1], c=data['Cluster'], cmap='viridis', s=50, alpha=0.7)
+plt.scatter(pca_components[outlier_indices, 0], pca_components[outlier_indices, 1], color='red', s=100, marker='x', label='Outlier-i')
+plt.xlabel('PCA1')
+plt.ylabel('PCA2')
+plt.title('PCA plot podataka sa outlier-ima')
+plt.colorbar(label='Klaster')
+plt.legend()
+plt.show()
+
+data_without_outliers = data.drop(outlier_indices)
+
+print("Podaci bez outlier-a:")
+print(data_without_outliers.head())
